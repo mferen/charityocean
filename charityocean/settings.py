33bec6fe1e33a5d84c43a4571c9984ded6d05105
mferen <mferen03@gmail.com>
2023-02-11 19:09:00 +0300

    update settings.py

--- charityocean/settings.py
+++ charityocean/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['0.0.0.0','127.0.0.1']
+ALLOWED_HOSTS = ['0.0.0.0','127.0.0.1','localhost','helptheworld.onrender.com']
 
 
 # Application definition
